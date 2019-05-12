b15d513bc7c40208db55e3cc6b36fcac99f1edc7
Evgen <evgeniyap88@gmail.com>
2019-05-12 21:36:22 +0500

    add client

--- src/Api/Controllers/IdentityController.cs
+++ src/Api/Controllers/IdentityController.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("/[controller]")]
     [ApiController]
+    [Authorize]
     public class IdentityController : ControllerBase
     {
         [HttpGet]
---
+++ src/Client/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace Client
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var token = GetToken("http://localhost:5000").GetAwaiter().GetResult();
+            Console.WriteLine(token.Json);
+            RunClient(token.AccessToken, "http://localhost:5001/identity").GetAwaiter().GetResult();
+            Console.ReadKey();
+        }
+
+        private static async Task<TokenResponse> GetToken(string authAddress)
+        {
+            var client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync(authAddress);
+
+            if (disco.IsError)
+            {
+                throw new Exception(disco.Error); ;
+                
+            }
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                throw new Exception(tokenResponse.Error);
+            }
+
+            return tokenResponse;
+        }
+
+        private static async Task RunClient(string accessToken, string reqAddress)
+        {
+            var client = new HttpClient();
+            client.SetBearerToken(accessToken);
+
+            var response = await client.GetAsync(reqAddress);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception(response.RequestMessage.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
